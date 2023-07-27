d529b9deb6c493c4add4dae048f228736bbf2f31
AthereoAndromeda <athereoandromeda@gmail.com>
2023-07-27 12:08:35 +0800

    refactor: Add more debug info

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,22 @@ pub async fn read_file<P: AsRef<Path>>(path: P) -> io::Result<Vec<String>> {
 
     let mut words = Vec::new();
 
+    #[cfg(debug_assertions)]
+    let mut line_count = 0;
+
     log::debug!("Iterating Lines");
     while let Some(line) = lines.next_line().await? {
         words.push(line);
+
+        #[cfg(debug_assertions)]
+        {
+            line_count += 1;
+        }
     }
+    log::debug!("Finished iterating lines");
+
+    #[cfg(debug_assertions)]
+    log::debug!("Lines Counted: {}", line_count);
 
     Ok(words)
 }
