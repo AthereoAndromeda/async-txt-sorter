bcaa164d845ecac4fec5e9b9c43b0869c8a5d531
AthereoAndromeda <athereoandromeda@gmail.com>
2023-08-01 15:07:01 +0800

    test(standard): Add unit tests for standard

--- src/standard.rs
+++ src/standard.rs
@@ -46,3 +46,34 @@ pub async fn sort(
     log::info!("Finished!");
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+    use std::io::Cursor;
+
+    use super::{read, sort};
+
+    #[tokio::test]
+    async fn read_test() {
+        let expected = vec!["b".to_string(), "a".to_string(), "c".to_string()];
+        let reader = Cursor::new("b\na\nc");
+
+        let a = read(reader).await.unwrap();
+
+        assert_eq!(a, expected);
+    }
+
+    #[tokio::test]
+    async fn sort_test() {
+        let input = vec!["b".to_string(), "a".to_string(), "c".to_string()];
+
+        let mut writer = Cursor::new(Vec::with_capacity(std::mem::size_of::<String>() * 3 * 2));
+        sort(input, &mut writer).await.unwrap();
+
+        let inner = writer.into_inner();
+
+        let expected = "a\nb\nc".to_string();
+        let processed = String::from_utf8_lossy(&inner).to_string();
+        assert_eq!(processed, expected);
+    }
+}
